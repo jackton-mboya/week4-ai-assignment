34ebdf3fc45984d0527bc27a89b38bac9370edda
jackton-mboya <150933562+jackton-mboya@users.noreply.github.com>
2026-03-11 09:41:31 +0300

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,60 @@
+# Question 4:
+Write a Python program that prints the following message:
+Hello AI Students
+print("Hello AI Students")
+
+
+# Question 5:
+Create the following variables in Python:
+name
+age
+city
+Print them so the output looks like this:
+
+Name: John
+Age: 20
+City: Nairobi
+name = "John"
+age = 20
+city = "Nairobi"
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+
+
+# Question 6:
+Create a list of five programming languages.
+
+Example:
+["Python", "Java", "C++", "JavaScript", "Go"]
+
+Print the first language in the list.
+  
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+# Print the first language
+print(languages[0])
+
+# Question 7:
+Create a dictionary  with the following student information:
+Name
+Age
+Course
+Print the student's name.
+
+Example dictionary format:
+student = {
+"name": "John",
+"age": 21,
+"course": "AI"
+}
+
+
+student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+# Print the student's name
+print(student["name"])
+
